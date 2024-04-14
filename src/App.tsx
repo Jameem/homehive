23df744ab47d65f548865bf39ee6b48fe1422ccd
Jameem <jameemkurikkal.mp@gmail.com>
2024-04-14 18:30:45 +0200

    feat: add navbar and connect button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { Navbar } from './components/Navbar';
+import { Properties } from './components/property/Index';
+
+const ethers = require('ethers');
 
 function App() {
+  const [account, setAccount] = useState('');
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const provider = new ethers.BrowserProvider(window.ethereum);
+
+        window.ethereum.on('accountsChanged', async () => {
+          const accounts = await window.ethereum?.request({
+            method: 'eth_requestAccounts',
+          });
+
+          const account = ethers.getAddress(accounts[0]);
+          setAccount(account);
+        });
+      } catch (error) {}
+    };
+    loadData();
+  }, [account]);
+
+  const onClickConnect = async () => {
+    try {
+      if (window?.ethereum) {
+        const accounts = await window.ethereum?.request({
+          method: 'eth_requestAccounts',
+        });
+        const account = ethers.getAddress(accounts[0]);
+        setAccount(account);
+      }
+    } catch (error) {}
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container px-1'>
+      <Navbar account={account} onConnect={onClickConnect} />
+      <Properties />
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,41 @@
+export const Navbar = ({
+  account,
+  onConnect,
+}: {
+  account: string;
+  onConnect: () => Promise<void>;
+}) => {
+  return (
+    <nav className='navbar'>
+      <div className='flex gap-1 align-center'>
+        <ul className='navbar__ul'>
+          <li>
+            <a href='/buy' className='navbar__anchor'>
+              Buy
+            </a>
+          </li>
+          <li>
+            <a href='/rent' className='navbar__anchor'>
+              Rent
+            </a>
+          </li>
+          <li>
+            <a href='/sell' className='navbar__anchor'>
+              Sell
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className='navbar__title'>
+        <span>Home</span>
+        <img src='logo.svg' width={40} alt='homehive' />
+        <span style={{ color: '#6563ff' }}>Hive</span>
+      </div>
+
+      <button className='button' onClick={onConnect}>
+        {account ? account : 'Connect Metamask'}
+      </button>
+    </nav>
+  );
+};

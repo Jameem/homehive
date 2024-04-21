ecb3eaf697afad31ab54ab18ec698823ac56d6d8
Jameem <jameemkurikkal.mp@gmail.com>
2024-04-21 09:29:07 +0200

    fix: update counter

--- contracts/Property.sol
+++ contracts/Property.sol
@@ -5,22 +5,24 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
+import "hardhat/console.sol";
+
 
 contract Property is ERC721URIStorage {
-    uint256 private _tokenIdCounter=1;
+    uint256 private _tokenIdCounter=0;
 
     constructor() ERC721("Property", "HHV") {}
 
-    function mint() public returns (uint256) {
+    function mint(string memory tokenURI) public returns (uint256) {
+
+        _tokenIdCounter += 1;
 
         uint256 tokenId = _tokenIdCounter;
 
         _mint(msg.sender, tokenId);
 
+        _setTokenURI(tokenId, tokenURI);
 
-        _setTokenURI(tokenId, Strings.toString(tokenId));
-
-        _tokenIdCounter += 1;
 
         return tokenId;
     }
@@ -28,4 +30,9 @@ contract Property is ERC721URIStorage {
     function totalSupply() public view returns (uint256) {
         return _tokenIdCounter;
     }
+
+    function getTokenURI(uint256 tokenId) public view returns(string memory) {
+        return tokenURI(tokenId);
+    }
+    
 }
